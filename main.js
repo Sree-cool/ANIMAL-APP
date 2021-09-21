76bd05f30cedbdf3c83f1a6355e7d24237ccae1b
Sree-cool <78275933+Sree-cool@users.noreply.github.com>
2021-09-20 21:46:27 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/NRkK9OTif/model.json',modelReady);
+}
+
+function modelReady() {
+    classifier.classify(gotResult);
+}
+
+function gotResult(error, results) {
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
+}
